1c6733fc38bf9f652fc68fcfcc288fe2cc766508
Manh Do - Mac <manhcntt2.1@gmail.com>
2024-05-19 23:17:47 +0700

    placeholder when no notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -25,6 +25,7 @@
         :note="note"
         @delete-clicked="handleDeleteClicked"
       />
+      <div v-if="!storeNotes.notes.length" class="is-size-4 has-text-centered has-text-grey-light is-family-monospace py-6">No notes here yet ...</div>
     </template>
   </div>
 </template>
